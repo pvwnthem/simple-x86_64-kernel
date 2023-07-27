581399b36a7e8f6c3045e796300470cbf42150aa
pvwnthem <pvwn.them@gmail.com>
2023-07-27 02:09:24 -0400

    finish header.s

--- src/impl/x86_64/boot/header.s
+++ src/impl/x86_64/boot/header.s
@@ -1,3 +1,15 @@
+section .multiboot_header
 header_start:
+    dd 0xe85250d6 ; multiboot
+    ; arch
+    dd 0 ; i386 (0x00)
+    ; header length
+    dd header_end - header_start
+    ; checksum
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+    ; end tag
+    dw 0
+    dw 0
+    dd 8
 
 header_end:
